ef086663df51b9acb37d1d42f8a851c36b6c5a37
ivanramos242 <ivanramos242@gmail.com>
2026-03-21 16:31:34 +0800

    Fix top spacing in assistant panel

--- App.tsx
+++ App.tsx
@@ -1125,7 +1125,7 @@ function AssistantPanel({
         </div>
       </div>
 
-      <div className="space-y-4 px-4 py-5 sm:px-6 sm:py-6">
+      <div className="space-y-4 px-4 pb-5 pt-0 sm:px-6 sm:pb-6 sm:pt-0">
         <div className="space-y-4">
           {status === 'connecting' ? (
             <div className="space-y-4">
